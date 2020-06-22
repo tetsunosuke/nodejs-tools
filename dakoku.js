c43e29315dbb6ca06098238c367905d411ea0b01
ITO Tetsunosuke <tetsunosuke.ito@photocreate.co.jp>
2020-06-22 15:21:01 +0900

    打刻ツール、green等

---
+++ dakoku.js
@@ -0,0 +1,49 @@
+const dateformat = require('dateformat');
+const config = require("config");
+const conf = config.get("kinrou");
+console.log(conf);
+
+
+const puppeteer = require('puppeteer');
+(async() => {
+    const browser = await puppeteer.launch({
+        headless: conf.headless,
+    });
+    // よく使うものは先に宣言しておくといいかも
+    let text;
+
+    const [page] = await browser.pages();
+    // トップページ
+    await page.goto(conf.url);
+
+    // ログイン。法人コード、社員コード、パスワードを入力し、ログインボタンを押します
+    await page.type('[name=houjinCode]', conf.houjinCode);
+    await page.type('[name=userId]', conf.userId);
+    await page.type('[name=password]', conf.password);
+    // ここは説明していませんが、次のページが読み込まれるまで待つという意味です
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click("#bt")
+    ]);
+
+    // 打刻
+    await page.click("[name='dakoku']");
+
+    const d = new Date();
+    const year = dateformat(d, 'yyyy');
+    const month = dateformat(d, 'mm');
+    const kijunDate = dateformat(d, 'yyyymmdd');
+
+    await page.goto(`https://kinrou.sas-cloud.jp/kinrou/dakokuList/index?syainCode=${kinrouConfig.userId}&year=${year}&month=${month}&kijunDate=${kijunDate}`);
+
+    let elms = await page.$$(".dakoku-all-list tr");
+    let elm;
+    text = "最後の打刻は：";
+    for (i of [3,4]) {
+        elm = await page.$(`.dakoku-all-list tr:nth-of-type(${elms.length-1}) td:nth-of-type(${i})`);
+        text += await page.evaluate(elm => elm.textContent, elm)
+    }
+    console.info(text)
+    // 閉じる
+    browser.close();
+})();
---
+++ green.js
@@ -0,0 +1,126 @@
+const config = require("config");
+const conf = config.get("green");
+const puppeteer = require('puppeteer');
+(async () => {
+    let selector;
+    let xpath;
+    let elm;
+    let elms;
+    let title;
+    const browser = await puppeteer.launch({
+        headless: conf.headless,
+        //devtools: true,
+    });
+    const [page] = await browser.pages();
+
+    // ログイン画面を開く
+    await Promise.all([
+        page.waitForNavigation(),
+        page.goto(conf.url),
+    ]);
+
+    // ID, パスワード入力、ログイン
+    await Promise.all([
+        page.waitForSelector("#client_cd"),
+        page.type("#client_cd", conf.userid),
+    ]);
+    // 上記が成立したら下記は続けてOK
+    await page.type("#client_password", conf.password);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click("input#btn_login"),
+    ]);
+
+
+    // TODO: アンケートが出た場合は閉じる必要がある
+    // elector: div#wrapper div.dialog_close.js-create_cookie
+    //
+    // 求職者検索ボタンをクリック
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click("#js-header-wrapper a:nth-child(2) > i")
+    ]);
+
+    // 検索条件リストをスクレイピング
+    selector = "div.client-navigation";
+    await page.waitForSelector(selector);
+
+    // 検索条件を引っ張って、その中から該当するものを選ぶ
+    selector = `${selector} > a > div:nth-of-type(1) > span`;
+    await page.waitForSelector(selector);
+    elms = await page.$$(selector);
+
+    // TODO: 1とかmdl-tag--round の要素を見つけ、それだけを取得したい
+    // //*[@id="tooltip-search-80830"]/div[2]/span[1] 
+
+    let text;
+    let indexes = [];
+    for (let i=0; i < elms.length; i++) {
+        text = await page.evaluate(elm => elm.textContent, elms[i]);
+        if (text.indexOf(conf.search) === 0) {
+            indexes.push(i+1);
+        }
+    }
+
+    // 気になる送信に関する対象URLを取得
+    let urls = [];
+    let matches;
+    let id;
+    for (let i of indexes) {
+        selector = `div.client-navigation > a:nth-of-type(${i})`;
+        elm = await page.$(selector);
+        text = await page.evaluate(elm => elm.href, elm);
+        /*
+        // href から #tooltip-search-${id} を mdl-tag--round を探してそれだけを抽出する
+        // https://www.green-japan.com/client/search/80818
+        matches = text.match(/\d+$/);
+        id = matches[0];
+        elm = await page.$(`#tooltip-search-${id} .mdl-tag--round`);
+        if (elm) {
+            // text = await page.evaluate(elm => elm.textContent, elm);
+            urls.push(text);
+        }
+        */
+        urls.push(text);
+    }
+
+    // 気になるを送信する
+    for (let url of urls) {
+        await Promise.all([
+            page.waitForNavigation(),
+            page.goto(url)
+        ]);
+        selector = "#js-main-contents h1";
+        await page.waitForSelector(selector);
+        elm = await page.$(selector);
+        title = await page.evaluate(elm => elm.textContent, elm);
+        let [name, entry] = title.split("link");
+
+        // こんな感じで数字がテキストとして取れる
+        // 1 - 100 / 149
+        // 0 / 0
+        // 1 - 1 / 1
+        elm = await page.$("#js-react-search-result");
+        // ここで数秒待たないと数字が更新されない！
+        await page.waitFor(conf.waitShort);
+        text = await page.evaluate(elm => elm.textContent, elm);
+        console.log(text, name, url);
+        if (text[0] !== "0") {
+            // 全て選択
+            selector = "#js-main-contents table tr > td > label";
+            await Promise.all([
+                page.waitForSelector(selector),
+                page.click(selector)
+            ]);
+            // 気になる送信ボタン
+            xpath = '//*[@id="js-main-contents"]/div/table/thead/tr/td[8]/div/div[1]/button';
+            await page.waitForXPath(xpath);
+            let elementHandleList = await page.$x(xpath);
+            await elementHandleList[0].click();
+            await page.waitFor(conf.waitLong);
+        }
+    }
+
+    browser.close();
+})();
